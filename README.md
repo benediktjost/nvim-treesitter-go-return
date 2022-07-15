32391da8fc7f1dc07ff059f539aef1fce206b372
benedikt.jost <benedikt.jost@freiheit.com>
2022-07-15 20:07:10 +0200

    polish code and add docu

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
 # nvim-treesitter-go-return
-This plugin uses treesitter to determine the return types of go function.
-This can be used to create a snippet that automatically returns the correct return values in case of an error.
+This plugin uses treesitter to determine the return types of a go function.
+It can be used to create snippets for autofilling the go if err!=nil pattern.
+
+## Usage with luasnip:
+
+local ls = require("luasnip")
+local snip = ls.snippet
+local text = ls.text_node
+local func = ls.function_node
+
+```
+local i = ls.insert_node
+local returnString = function()
+	local res = " return "
+	require("nvim-treesitter-go-return").setup({ buildIn = { string = "foo" }, useNil = false })
+	local values = require("nvim-treesitter-go-return").getReturnValues(true)
+	for _, val in pairs(values) do
+		res = res .. val
+		if val ~= "err" then
+			res = res .. ", "
+		end
+	end
+	return { res }
+end
+
+ls.add_snippets(nil, {
+	all = {
+		snip({
+			trig = "ie",
+		}, {
+			text({ "if " }),
+			i(1, "err"),
+			text({ " != nil {" }),
+			text({ " ", " " }),
+			func(returnString, {}),
+			func(function(args, _, user_arg_1)
+				return args [1] [1] .. user_arg_1
+			end, { 1 }, { user_args = { "" } }),
+			text({ " ", "}", "" }),
+		}),
+	},
+})
+```
+
+## Installation
+Use your favorite package manager
+
+
+## Configuration
+
+nvim-treesitter-go-return.setup{opts}
+Setup function to be run by user. Configures the default return values.
+The defaults are:
+```
+require("nvim-treesitter-go-return").setup({
+buildIn = { 
+	bool = '"false"',
+	string = '""',
+	int16 = "0",
+	uint16 = "0",
+	uint32 = "0",
+	uint64 = "0",
+	int64 = "0",
+	int = "0",
+	uint = "0",
+	unitptr = "0",
+	float = "0",
+	float32 = "0",
+	float64 = "0",
+	complex64 = "0",
+	complex128 = "0",
+},
+useNil = true,
+})
+```
--- lua/nvim-treesitter-go-return/init.lua
+++ lua/nvim-treesitter-go-return/init.lua
@@ -1,32 +1,86 @@
 local ts_utils = require("nvim-treesitter.ts_utils")
+
 local M = {}
 
-local parseToken = function(token)
-	local buildIn = {}
-	buildIn["string"] = '""'
-	buildIn["int"] = "0"
-	buildIn["float"] = "0"
-	-- TODO (byte), int16, uint16, int32 (rune), uint32, int64, uint64, int, uint, uintptr. float32, float64. complex64, complex128.
+M._buildIn = {}
+M._nilReturn = true
+M._buildIn = {
+	bool = '"false"',
+	string = '""',
+	int16 = "0",
+	uint16 = "0",
+	uint32 = "0",
+	uint64 = "0",
+	int64 = "0",
+	int = "0",
+	uint = "0",
+	unitptr = "0",
+	float = "0",
+	float32 = "0",
+	float64 = "0",
+	complex64 = "0",
+	complex128 = "0",
+}
 
-	if string.starts(token, "*") then
-		return "nil, "
+local parseToken = function(token, skipError)
+	local buildIn = M._buildIn
+	if string.starts(token, "*") or string.starts(token, "[]") then
+		if M._nilReturn then
+			return "nil"
+		end
+		return "&" .. buildIn[token] .. {}
+	end
+	if
+		string.starts(token, "map[")
+		or string.starts(token, "func")
+		or string.starts(token, "chan")
+		or string.starts(token, "interface")
+	then
+		return "nil"
 	end
 	if buildIn[token] then
-		return buildIn[token] .. ", "
+		return buildIn[token]
 	end
 	if token == "error" then
-		return "err"
+		if not skipError then
+			return "err"
+		end
+		return nil
 	end
-	return token .. "{},"
+	return token .. "{}"
 end
 
 function string.starts(String, Start)
 	return string.sub(String, 1, string.len(Start)) == Start
 end
 
+M.setup = function(opts)
+	for x in pairs(opts.buildIn) do
+		if M._buildIn[x] then
+			M._buildIn[x] = opts.buildIn[x]
+		end
+	end
+	if opts.useNil then
+		M._nilReturn = opts.useNil
+	end
+end
+
+M.getReturnValues = function(skipError)
+	local res = {}
+	for _, val in pairs(M.getReturnTypes()) do
+		local parsed = parseToken(val, skipError)
+		if parsed then
+			table.insert(res, parsed)
+		end
+	end
+	return res
+end
+
 M.getReturnTypes = function()
+	-- get the node at the cursor
 	local expr = ts_utils.get_node_at_cursor()
 
+	-- find the method or function node
 	while expr do
 		if expr:type() == "method_declaration" or expr:type() == "function_declaration" then
 			break
@@ -38,6 +92,7 @@ M.getReturnTypes = function()
 		print("no function or method found")
 	end
 
+	-- find the result node
 	local resultNode = nil
 	for node, field in expr:iter_children() do
 		if field == "result" then
@@ -50,11 +105,14 @@ M.getReturnTypes = function()
 	end
 
 	local result_types = {}
+	-- loop over all parameter_declaration
 	for node in resultNode:iter_children() do
 		if node:type() == "parameter_declaration" then
+			-- in case of an unnamed return
 			if node:named_child_count() == 1 then
 				table.insert(result_types, ts_utils.get_node_text(node:named_child())[1])
 			else
+				-- in case of namend returns
 				for _ = 1, node:named_child_count() - 1, 1 do
 					table.insert(
 						result_types,
@@ -70,9 +128,11 @@ end
 M.getResultString = function()
 	local res = "return "
 	for _, val in pairs(M.getReturnTypes()) do
-		res = res .. parseToken(val)
+		res = res .. parseToken(val, false)
 	end
 	return tostring(res)
 end
+
+M.setup({ buildIn = { string = "foo" }, useNil = false })
 
 return M
